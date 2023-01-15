1705e97bc8e29d2a0dd8c527b1453d2edbd573c5
evaw277 <evaweisneck@gmail.com>
2023-01-15 17:03:54 -0500

    fixed search forecast bug

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
 
@@ -10,6 +10,10 @@ export default function WeatherForecast(props) {
     setForecast(response.data.daily);
     setLoaded(true);
   }
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.city]);
 
   if (loaded) {
     console.log(forecast);
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import Weather from "./Weather";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./Weather.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
